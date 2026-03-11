792bb8fff40be7f5ebb46972317698daf6df9511
Ayush Goyal <ayush.goyal@wise.com>
2026-03-11 14:30:42 +0000

    tester endpoint change

--- server/src/main/java/com/ayush/googlecalendarnotifications/controller/Tester.java
+++ server/src/main/java/com/ayush/googlecalendarnotifications/controller/Tester.java
@@ -4,6 +4,7 @@ import com.ayush.googlecalendarnotifications.dto.Alert;
 import com.ayush.googlecalendarnotifications.dto.AlertType;
 import com.ayush.googlecalendarnotifications.service.AlertDispatcher;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +14,8 @@ public class Tester {
     public Tester(AlertDispatcher alertDispatcher) {this.alertDispatcher = alertDispatcher;}
 
     @PostMapping("/test/trigger")
-    public String trigger() {
-        alertDispatcher.wsNotify(new Alert("test", "Test Alert", AlertType.STARTING_NOW));
+    public String trigger(@RequestParam String key) {
+        alertDispatcher.wsNotify(new Alert(key, "Test Alert", AlertType.STARTING_NOW));
         return "Triggered alert dispatch!";
     }
 }
